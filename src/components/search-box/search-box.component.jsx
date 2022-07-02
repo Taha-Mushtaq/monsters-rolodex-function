18b71b507f983916a08bc52a9e2f2487b42fbea1
Taha-Mushtaq <71284345+Taha-Mushtaq@users.noreply.github.com>
2022-07-02 11:55:44 +0500

    deployed

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,16 @@
+
+import './search-box.styles.css'
+
+
+const SearchBox =({className,placeHolder,onChangeHandler})=>{
+  return(
+            <input
+          className={`search-box ${className}`}
+          type="search"
+          placeholder={placeHolder}
+          onChange={onChangeHandler}
+        />
+  )
+    }
+
+export default SearchBox
